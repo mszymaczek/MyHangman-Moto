ba9be154122faa632612c4ce9bb41a10b273ed1e
mszymaczek <michal.szymaczek@gmail.com>
2021-07-01 15:28:58 +0200

    Add project files.

---
+++ MyHangman Moto/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyHangman_Moto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The very first Hangman");
+            Console.WriteLine("Player name");
+
+            string playerName = Console.ReadLine();
+            Console.WriteLine("Witaj " + playerName);
+
+            Console.WriteLine("Rules are simple. You guess the secret word by guessing single letters.");
+            Console.WriteLine("Choose option:");
+            Console.WriteLine("1. New game");
+            Console.WriteLine("2. Leaderboard");
+            Console.WriteLine("3. End");
+
+            Console.ReadLine();
+        }
+    }
+}
