b7369ca3be7fcf98d02784b4f8167f07e084f436
mszymaczek <michal.szymaczek@gmail.com>
2021-07-02 12:14:51 +0200

    First approach to Hangman game Asking for secretword, guessed letters variable and filling it with guessed letters

--- MyHangman Moto/Program.cs
+++ MyHangman Moto/Program.cs
@@ -6,19 +6,47 @@ namespace MyHangman_Moto
     {
         static void Main(string[] args)
         {
+            char[] guessedWord;
+            char letter;
+
             Console.WriteLine("The very first Hangman");
-            Console.WriteLine("Player name");
+            Console.WriteLine("Write player name");
 
             string playerName = Console.ReadLine();
-            Console.WriteLine("Witaj " + playerName);
+            Console.WriteLine("Hello " + playerName);
 
             Console.WriteLine("Rules are simple. You guess the secret word by guessing single letters.");
-            Console.WriteLine("Choose option:");
-            Console.WriteLine("1. New game");
-            Console.WriteLine("2. Leaderboard");
-            Console.WriteLine("3. End");
+            Console.WriteLine("Ask someone to write the secret word - don't look! :)");
 
-            Console.ReadLine();
+            string secretWord = Console.ReadLine();
+
+            guessedWord = new char[secretWord.Length];
+
+            for (int i = 0; i < secretWord.Length; i++)
+            {
+                guessedWord[i] = '_';
+            }
+            Console.Clear();
+            Console.WriteLine(guessedWord);
+
+            Console.WriteLine("Start typing letters to guess the secret word. Good luck!");
+
+            letter = Console.ReadLine()[0];
+
+            Console.WriteLine("Your letter: " + letter);
+
+            for (int i = 0; i < secretWord.Length; i++)
+            {
+                if (letter == secretWord[i]) 
+                {
+                    guessedWord[i] = letter;
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine(guessedWord);
+
+
         }
     }
 }
